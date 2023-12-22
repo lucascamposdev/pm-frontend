101f5aa3c84e82de1e37cf4dd4bf12df20f3f36a
Lucas <lucaskmps@gmail.com>
2023-12-22 16:14:11 -0300

    more changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2020', // ou uma versão mais recente do ECMAScript, como 'esnext'
+    target: 'esnext', // ou uma versão mais recente do ECMAScript, como 'esnext'
   },
 })
