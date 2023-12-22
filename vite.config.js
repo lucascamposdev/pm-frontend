cf3320ddcfc37dec483ab93b9ec58071072dc0a1
Lucas <lucaskmps@gmail.com>
2023-12-22 16:16:11 -0300

    more adjustments

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext', // ou uma versão mais recente do ECMAScript, como 'esnext'
+    target: 'esnext',
   },
 })
