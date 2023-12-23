b300a7b2dc50a935ac3066aef76fee9b8490367e
Lucas <lucaskmps@gmail.com>
2023-12-23 14:30:40 -0300

    task page adjustments

--- src/pages/your-tasks/Tasks.jsx
+++ src/pages/your-tasks/Tasks.jsx
@@ -1,22 +1,14 @@
 import styles from './Tasks.module.css'
 
 // Hooks
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 // Reducer
-import { getuser } from '../../reducers/userReducer'
 import Table from '../../components/shared/Table/Table'
 
 const Tasks = () => {
-  const { auth } = useSelector(state => state.authReducer)
-  const { user, profile } = useSelector(state => state.userReducer)
+  const { user } = useSelector(state => state.userReducer)
   const { task } = useSelector(state => state.taskReducer)
-  const dispatch = useDispatch();
-
-  useEffect(() =>{
-      dispatch(getuser(auth.id))
-  }, [task])
 
   return (
     <div className='page'>
